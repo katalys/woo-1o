30bd61b7c0c9bfa97ddf6f4dbae8d898a23d7736
Tiago Tescaro <tiago@atom6studio.com>
2023-12-07 23:35:24 -0300

    WOO-21 - Fix problem with export

--- trunk/assets/inc/AdvertiserIntegration/export_orders.php
+++ trunk/assets/inc/AdvertiserIntegration/export_orders.php
@@ -26,7 +26,7 @@ function run()
   }
 
   // require WP bootstrap
-  require_once ABSPATH . 'wp-load.php';
+  require_once __DIR__ . '/../../../../../../wp-load.php';
   require_once __DIR__ . '/admin_func.php';
 
   ini_set('display_errors', 1);
